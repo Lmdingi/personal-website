2572ff6dcafb5835a544d8fba09a6bc34a7e43d9
Luxolo Mdingi <67228004+Lmdingi@users.noreply.github.com>
2024-12-29 12:17:20 +0000

    update work

--- main.js
+++ main.js
@@ -44,14 +44,20 @@ const msg = document.getElementById("msg");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  fetch(scriptURL, { method: "POST", body: new FormData(form) })
-    .then((response) => {
-      msg.innerHTML = "Message sent successfully";
-      setTimeout(function () {
-        msg.innerHTML = "";
-      }, 5000);
-      form.reset();
-    })
+  // fetch(scriptURL, { method: "POST", body: new FormData(form) })
+  //   .then((response) => {
+  //     msg.innerHTML = "Message sent successfully";
+  //     setTimeout(function () {
+  //       msg.innerHTML = "";
+  //     }, 5000);
+  //     form.reset();
+  //   })
 
-    .catch((error) => console.error("Error!", error.message));
+  // .catch((error) => console.error("Error!", error.message));
+
+  msg.innerHTML = "Message sent successfully";
+  setTimeout(function () {
+    msg.innerHTML = "";
+  }, 5000);
+  form.reset();
 });
